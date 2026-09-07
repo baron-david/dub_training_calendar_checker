b39a3eb503fae5474c4378ba4b906ae5da6e5457
baron-david <48170946+baron-david@users.noreply.github.com>
2026-09-07 15:53:25 -0700

    minor update to file name

--- check_appointments_simple_dates_any.py
+++ check_appointments_simple_dates_any.py
@@ -31,7 +31,7 @@ TIMEZONE = "America/Los_Angeles"
 BASE_URL = "https://dubtraining.as.me/api/scheduling/v1/availability/month"
 TIMES_URL = "https://dubtraining.as.me/api/scheduling/v1/availability/times"
 
-STATE_FILE = Path(os.environ.get("STATE_FILE", "known_dates.json"))
+STATE_FILE = Path(os.environ.get("STATE_FILE", "known_dates_debug.json"))
 SLACK_WEBHOOK_URL = os.environ.get("NOTIFY_WEBHOOK_URL")
 GROUPME_BOT_ID = os.environ.get("GROUPME_BOT_ID")
 MONTHS_AHEAD = int(os.environ.get("MONTHS_AHEAD", "3"))
